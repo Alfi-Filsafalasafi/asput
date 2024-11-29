85d317a07ad62a688cf40cc87eb89c46d31a2251
Alfi-Filsafalasafi <filsafalasafi@gmail.com>
2024-11-29 22:36:04 +0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const navbar = document.querySelector(".navbar");
+  
+    window.addEventListener("scroll", () => {
+      const currentScroll = window.pageYOffset;
+  
+      if (currentScroll > 60) {
+        // Munculkan navbar jika jarak scroll lebih dari 100px
+        navbar.classList.remove("navbar-hidden");
+        navbar.classList.add("navbar-visible");
+      } else {
+        // Sembunyikan navbar jika kurang dari 100px
+        navbar.classList.remove("navbar-visible");
+        navbar.classList.add("navbar-hidden");
+      }
+    });
+  });
+  
+    const decrementBtn = document.getElementById('decrement');
+    const incrementBtn = document.getElementById('increment');
+    const quantityInput = document.getElementById('quantity');
+
+    decrementBtn.addEventListener('click', () => {
+      let currentValue = parseInt(quantityInput.value);
+      if (currentValue > 1) {
+        quantityInput.value = currentValue - 1;
+      }
+    });
+
+    incrementBtn.addEventListener('click', () => {
+      let currentValue = parseInt(quantityInput.value);
+      quantityInput.value = currentValue + 1;
+    });
