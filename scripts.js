570b590979aa8cfb6acbbe7984472c08d5082ba7
Alfi-Filsafalasafi <filsafalasafi@gmail.com>
2024-12-03 13:32:45 +0700

    fix : end revisi

--- scripts.js
+++ scripts.js
@@ -20,6 +20,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const decrementBtn = document.getElementById('decrement');
   const incrementBtn = document.getElementById('increment');
   const quantityInput = document.getElementById('quantity');
+
+  const decrementBtn1 = document.getElementById('decrement1');
+  const incrementBtn1 = document.getElementById('increment1');
+  const quantityInput1 = document.getElementById('quantity1');
   
   if (decrementBtn && incrementBtn && quantityInput) {
     decrementBtn.addEventListener('click', () => {
@@ -32,6 +36,20 @@ document.addEventListener("DOMContentLoaded", () => {
     incrementBtn.addEventListener('click', () => {
       let currentValue = parseInt(quantityInput.value);
       quantityInput.value = currentValue + 1;
+    });
+  }
+
+  if (decrementBtn1 && incrementBtn1 && quantityInput1) {
+    decrementBtn1.addEventListener('click', () => {
+      let currentValue = parseInt(quantityInput1.value);
+      if (currentValue > 1) {
+        quantityInput1.value = currentValue - 1;
+      }
+    });
+  
+    incrementBtn1.addEventListener('click', () => {
+      let currentValue = parseInt(quantityInput1.value);
+      quantityInput1.value = currentValue + 1;
     });
   }
   
